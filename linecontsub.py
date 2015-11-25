16498a85e44d224182bfe39887d51fb53cfbb7ef
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-25 12:11:32 +0100

    merge version!

--- linecontsub.py
+++ linecontsub.py
@@ -1,48 +1,74 @@
 import os
 if not os.path.exists('test_frequency.image.fits'):
-    os.system('rm -rf w51_test_small.ms')
-    concat(vis=['w51_test_small_spw3.ms','w51_test_small_spw7.ms'],
-           concatvis='w51_test_small.ms')
+    os.system('rm -rf w51_test_merge_spw3_copy.ms')
+    os.system('cp -r w51_test_merge_spw3.ms w51_test_merge_spw3_copy.ms')
 
     os.system('rm -rf test_frequency.*')
-    clean(vis='w51_test_small.ms', imagename="test_frequency",
+    clean(vis='w51_test_merge_spw3_copy.ms', imagename="test_frequency",
           field="", spw='', mode='channel', outframe='LSRK',
           interpolation='linear', imagermode='mosaic', interactive=False,
-          niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
+          niter=50000, threshold='2.0mJy', imsize=[512,512], cell='0.052arcsec',
           weighting='briggs', phasecenter='J2000 19h23m43.905 +14d30m28.08',
-          pbcor=False, usescratch=True, robust=1.0)
+          pbcor=False, usescratch=False, robust=1.0)
     exportfits('test_frequency.image', 'test_frequency.image.fits', dropdeg=True, overwrite=True)
     exportfits('test_frequency.model', 'test_frequency.model.fits', dropdeg=True, overwrite=True)
     for suffix in ('model','flux','psf','residual'):
         os.system('rm -rf test_frequency.{0}'.format(suffix))
 
+    os.system('rm -rf test_mfs.*')
+    clean(vis='w51_test_merge_spw3_copy.ms', imagename="test_mfs",
+          field="", spw='', mode='mfs', outframe='LSRK',
+          interpolation='linear', imagermode='mosaic', interactive=False,
+          niter=10000, threshold='5.0mJy', imsize=[512,512], cell='0.052arcsec',
+          weighting='briggs', phasecenter='J2000 19h23m43.905 +14d30m28.08',
+          pbcor=False, usescratch=False, robust=1.0)
+    exportfits('test_mfs.image', 'test_mfs.image.fits', dropdeg=True, overwrite=True)
+    exportfits('test_mfs.model', 'test_mfs.model.fits', dropdeg=True, overwrite=True)
+    for suffix in ('model','flux','psf','residual'):
+        os.system('rm -rf test_mfs.{0}'.format(suffix))
+
+
 import numpy as np
 import spectral_cube
 from astropy import units as u
-cube = spectral_cube.SpectralCube.read('test_frequency.image.fits').with_spectral_unit(u.Hz)
-cont = cube.min(axis=0)
-contsub = cube-cont
-# temporary hack for issue #251
-contsub._data = contsub._data.value
-contsub = contsub.with_mask(contsub>15*u.mJy)
-ppbeam = np.abs((cube.beam.sr / (cube.wcs.pixel_scale_matrix[0,0]*cube.wcs.pixel_scale_matrix[1,1]*u.deg**2)).decompose())
-hdu = contsub.hdu
+# cube = spectral_cube.SpectralCube.read('test_frequency.image.fits').with_spectral_unit(u.Hz)
+# cont = cube.min(axis=0)
+# contsub = cube-cont
+# # temporary hack for issue #251
+# contsub._data = contsub._data.value
+# contsub = contsub.with_mask(contsub>15*u.mJy)
+# ppbeam = np.abs((cube.beam.sr / (cube.wcs.pixel_scale_matrix[0,0]*cube.wcs.pixel_scale_matrix[1,1]*u.deg**2)).decompose())
+# hdu = contsub.hdu
+# # this scaling may be necessary if setjy is used
+# # I found that the cube version was about 70x too low, maybe worse...
+# #hdu.data *= ppbeam # because apparently CASA divides by this?
+# hdu.writeto('test_contsub_cube.fits', clobber=True)
+# header = contsub.header
+cubedata = fits.getdata('test_frequency.image.fits')
+#cube = spectral_cube.SpectralCube.read('test_frequency.image.fits')
+cont = np.percentile(cubedata[1:-1,:,:], 10, axis=0)
+contsub = cubedata - cont
+#cont = cube.min(axis=0)
+#ppbeam = np.abs((cube.beam.sr / (cube.wcs.pixel_scale_matrix[0,0]*cube.wcs.pixel_scale_matrix[1,1]*u.deg**2)).decompose())
+header = cubeheader = fits.getheader('test_frequency.image.fits')
+#hdu = cont.hdu
+#ppbeam = header['BMAJ']*header['BMIN']*2*np.pi/2.35**2/header['CDELT2']**2
+hdu = fits.PrimaryHDU(data=cont, header=header)
 # this scaling may be necessary if setjy is used
-# I found that the cube version was about 70x too low, maybe worse...
 #hdu.data *= ppbeam # because apparently CASA divides by this?
 hdu.writeto('test_contsub_cube.fits', clobber=True)
-header = contsub.header
+
 importfits('test_contsub_cube.fits', 'test_contsub_cube.image',
            overwrite=True, defaultaxes=T,
            defaultaxesvalues=[header['CRVAL1'], header['CRVAL2'],
                               header['CRVAL3'], 'I'])
 
-os.system('rm -rf w51_test_small_linecubesub.ms')
-split('w51_test_small.ms', 'w51_test_small_linecubesub.ms', datacolumn='data')
+os.system('rm -rf w51_test_merge_spw3_copy_linecubesub.ms')
+split('w51_test_merge_spw3_copy.ms', 'w51_test_merge_spw3_copy_linecubesub.ms', datacolumn='data')
 
 # os.system('rm -rf test_frequency_linecubesub_dirty.*')
 # # try using clean to populate the model column
-# clean(vis='w51_test_small_linecubesub.ms', imagename="test_frequency_linecubesub_dirty",
+# clean(vis='w51_test_merge_spw3_copy_linecubesub.ms', imagename="test_frequency_linecubesub_dirty",
 #       field="", spw='', mode='channel', outframe='LSRK',
 #       interpolation='linear', imagermode='mosaic', interactive=False,
 #       modelimage='test_contsub_cube.image',
@@ -53,7 +79,7 @@ split('w51_test_small.ms', 'w51_test_small_linecubesub.ms', datacolumn='data')
 # for suffix in ('image','model','flux','psf','residual'):
 #     os.system('rm -rf test_frequency_linecubesub_dirty.{0}'.format(suffix))
 
-im.open('w51_test_small_linecubesub.ms')
+im.open('w51_test_merge_spw3_copy_linecubesub.ms')
 from astropy import coordinates
 c = coordinates.SkyCoord(header['CRVAL1'], header['CRVAL2'], unit=('deg','deg'), frame='fk5')
 im.defineimage(nx=cube.shape[1],
@@ -69,16 +95,16 @@ im.makemodelfromsd(sdimage='test_contsub_cube.image', modelimage='model_contsubc
 im.ft(model='model_contsubcube')
 im.close()
 # setjy does some strange scaling
-#setjy(vis='w51_test_small_linecubesub.ms', model='model_test', usescratch=True,
+#setjy(vis='w51_test_merge_spw3_copy_linecubesub.ms', model='model_test', usescratch=True,
 #      field='', standard='manual')
 #
 # ft claims there is no frequency overlap between the image and the data
-#ft(vis='w51_test_small_linecubesub.ms', model='test_continuum_min.image', usescratch=True, nterms=1)
+#ft(vis='w51_test_merge_spw3_copy_linecubesub.ms', model='test_continuum_min.image', usescratch=True, nterms=1)
 
-uvsub('w51_test_small_linecubesub.ms')
+uvsub('w51_test_merge_spw3_copy_linecubesub.ms')
 
 os.system('rm -rf test_frequency_linecubesub.*')
-clean(vis='w51_test_small_linecubesub.ms', imagename="test_frequency_linecubesub",
+clean(vis='w51_test_merge_spw3_copy_linecubesub.ms', imagename="test_frequency_linecubesub",
       field="", spw='', mode='channel', outframe='LSRK',
       interpolation='linear', imagermode='mosaic', interactive=False,
       niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
@@ -88,3 +114,4 @@ exportfits('test_frequency_linecubesub.image', 'test_frequency_linecubesub.image
 exportfits('test_frequency_linecubesub.model', 'test_frequency_linecubesub.model.fits', dropdeg=True, overwrite=True)
 for suffix in ('image','model','flux','psf','residual'):
     os.system('rm -rf test_frequency_linecubesub.{0}'.format(suffix))
+
--- mincontsub.py
+++ mincontsub.py
@@ -1,42 +1,68 @@
 if not os.path.exists('test_frequency.image.fits'):
-    os.system('rm -rf w51_test_small.ms')
-    concat(vis=['w51_test_small_spw3.ms','w51_test_small_spw7.ms'],
-           concatvis='w51_test_small.ms')
+    os.system('rm -rf w51_test_merge_spw3_copy.ms')
+    os.system('cp -r w51_test_merge_spw3.ms w51_test_merge_spw3_copy.ms')
 
     os.system('rm -rf test_frequency.*')
-    clean(vis='w51_test_small.ms', imagename="test_frequency",
+    clean(vis='w51_test_merge_spw3_copy.ms', imagename="test_frequency",
           field="", spw='', mode='channel', outframe='LSRK',
           interpolation='linear', imagermode='mosaic', interactive=False,
-          niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
+          niter=50000, threshold='2.0mJy', imsize=[512,512], cell='0.052arcsec',
           weighting='briggs', phasecenter='J2000 19h23m43.905 +14d30m28.08',
-          pbcor=False, usescratch=True, robust=1.0)
+          pbcor=False, usescratch=False, robust=1.0)
     exportfits('test_frequency.image', 'test_frequency.image.fits', dropdeg=True, overwrite=True)
     exportfits('test_frequency.model', 'test_frequency.model.fits', dropdeg=True, overwrite=True)
     for suffix in ('model','flux','psf','residual'):
         os.system('rm -rf test_frequency.{0}'.format(suffix))
 
+    os.system('rm -rf test_mfs.*')
+    clean(vis='w51_test_merge_spw3_copy.ms', imagename="test_mfs",
+          field="", spw='', mode='mfs', outframe='LSRK',
+          interpolation='linear', imagermode='mosaic', interactive=False,
+          niter=10000, threshold='5.0mJy', imsize=[512,512], cell='0.052arcsec',
+          weighting='briggs', phasecenter='J2000 19h23m43.905 +14d30m28.08',
+          pbcor=False, usescratch=False, robust=1.0)
+    exportfits('test_mfs.image', 'test_mfs.image.fits', dropdeg=True, overwrite=True)
+    exportfits('test_mfs.model', 'test_mfs.model.fits', dropdeg=True, overwrite=True)
+    for suffix in ('model','flux','psf','residual'):
+        os.system('rm -rf test_mfs.{0}'.format(suffix))
+
+
 import numpy as np
 import spectral_cube
 from astropy import units as u
-cube = spectral_cube.SpectralCube.read('test_frequency.image.fits')
-cont = cube.min(axis=0)
-ppbeam = np.abs((cube.beam.sr / (cube.wcs.pixel_scale_matrix[0,0]*cube.wcs.pixel_scale_matrix[1,1]*u.deg**2)).decompose())
-hdu = cont.hdu
+from astropy.io import fits
+from astropy import wcs
+cubedata = fits.getdata('test_frequency.image.fits')
+#cube = spectral_cube.SpectralCube.read('test_frequency.image.fits')
+cont = np.percentile(cubedata[1:-1,:,:], 10, axis=0)
+#cont = cube.min(axis=0)
+#ppbeam = np.abs((cube.beam.sr / (cube.wcs.pixel_scale_matrix[0,0]*cube.wcs.pixel_scale_matrix[1,1]*u.deg**2)).decompose())
+cubeheader = fits.getheader('test_frequency.image.fits')
+w = wcs.WCS(cubeheader)
+#hdu = cont.hdu
+header = flatheader = w.sub([wcs.WCSSUB_CELESTIAL]).to_header()
+flatheader['BUNIT'] = cubeheader['BUNIT']
+header['RESTFRQ'] = float(header['RESTFRQ'])
+header['BMAJ'] = cubeheader['BMAJ']
+header['BMIN'] = cubeheader['BMIN']
+header['BPA'] = cubeheader['BPA']
+#ppbeam = header['BMAJ']*header['BMIN']*2*np.pi/2.35**2/header['CDELT2']**2
+hdu = fits.PrimaryHDU(data=cont, header=flatheader)
 # this scaling may be necessary if setjy is used
 #hdu.data *= ppbeam # because apparently CASA divides by this?
 hdu.writeto('test_continuum_min.fits', clobber=True)
-header = cont.header
+#header = cont.header
 importfits('test_continuum_min.fits', 'test_continuum_min.image',
            overwrite=True, defaultaxes=T,
            defaultaxesvalues=[header['CRVAL1'], header['CRVAL2'],
-                              cube.header['CRVAL3'], 'I'])
+                              cubeheader['CRVAL3'], 'I'])
 
-os.system('rm -rf w51_test_small_imcont.ms')
-split('w51_test_small.ms', 'w51_test_small_imcont.ms', datacolumn='data')
+os.system('rm -rf w51_test_merge_spw3_copy_imcont.ms')
+split('w51_test_merge_spw3_copy.ms', 'w51_test_merge_spw3_copy_imcont.ms', datacolumn='data')
 
 # # try using clean to populate the model column
 # os.system('rm -rf test_frequency_mincontsub_dirty.*')
-# clean(vis='w51_test_small_imcont.ms', imagename="test_frequency_mincontsub_dirty",
+# clean(vis='w51_test_merge_spw3_copy_imcont.ms', imagename="test_frequency_mincontsub_dirty",
 #       modelimage='test_continuum_min.image',
 #       field="", spw='', mode='channel', outframe='LSRK',
 #       interpolation='linear', imagermode='mosaic', interactive=False,
@@ -47,10 +73,10 @@ split('w51_test_small.ms', 'w51_test_small_imcont.ms', datacolumn='data')
 # for suffix in ('image','model','flux','psf','residual'):
 #     os.system('rm -rf test_frequency_mincontsub_dirty.{0}'.format(suffix))
 
-im.open('w51_test_small_imcont.ms')
+im.open('w51_test_merge_spw3_copy_imcont.ms')
 from astropy import coordinates
 c = coordinates.SkyCoord(header['CRVAL1'], header['CRVAL2'], unit=('deg','deg'), frame='fk5')
-im.defineimage(nx=cube.shape[1],
+im.defineimage(nx=cubedata.shape[1],
                cellx='{0}arcsec'.format(header['CDELT2']*3600),
                phasecenter='J2000 {0} {1}'.format(c.ra.to_string(),
                                                   c.dec.to_string())),
@@ -60,16 +86,16 @@ im.makemodelfromsd(sdimage='test_continuum_min.image', modelimage='model_test')
 im.ft(model='model_test')
 im.close()
 # setjy does some strange scaling
-#setjy(vis='w51_test_small_imcont.ms', model='model_test', usescratch=True,
+#setjy(vis='w51_test_merge_spw3_copy_imcont.ms', model='model_test', usescratch=True,
 #      field='', standard='manual')
 #
 # ft claims there is no frequency overlap between the image and the data
-#ft(vis='w51_test_small_imcont.ms', model='test_continuum_min.image', usescratch=True, nterms=1)
+#ft(vis='w51_test_merge_spw3_copy_imcont.ms', model='test_continuum_min.image', usescratch=True, nterms=1)
 
-uvsub('w51_test_small_imcont.ms')
+uvsub('w51_test_merge_spw3_copy_imcont.ms')
 
 os.system('rm -rf test_frequency_mincontsub.*')
-clean(vis='w51_test_small_imcont.ms', imagename="test_frequency_mincontsub",
+clean(vis='w51_test_merge_spw3_copy_imcont.ms', imagename="test_frequency_mincontsub",
       field="", spw='', mode='channel', outframe='LSRK',
       interpolation='linear', imagermode='mosaic', interactive=False,
       niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
@@ -85,16 +111,16 @@ for suffix in ('flux','psf','residual'):
 # We're calling this imcont-linecont, which is to be read "imcont-then-linecont"
 # in principle, this should be a nice continuum-only cube....
 
-os.system('rm -rf w51_test_small_imcont-linecont.ms')
-split('w51_test_small.ms', 'w51_test_small_imcont-linecont.ms', datacolumn='data')
+os.system('rm -rf w51_test_merge_spw3_copy_imcont-linecont.ms')
+split('w51_test_merge_spw3_copy.ms', 'w51_test_merge_spw3_copy_imcont-linecont.ms', datacolumn='data')
 
-im.open('w51_test_small_imcont-linecont.ms')
+im.open('w51_test_merge_spw3_copy_imcont-linecont.ms')
 im.ft(model='test_frequency_mincontsub.model')
 im.close()
-uvsub('w51_test_small_imcont-linecont.ms')
+uvsub('w51_test_merge_spw3_copy_imcont-linecont.ms')
 
 os.system('rm -rf test_frequency_mincontsub-linecontsub.*')
-clean(vis='w51_test_small_imcont-linecont.ms', imagename="test_frequency_mincontsub-linecontsub",
+clean(vis='w51_test_merge_spw3_copy_imcont-linecont.ms', imagename="test_frequency_mincontsub-linecontsub",
       field="", spw='', mode='channel', outframe='LSRK',
       interpolation='linear', imagermode='mosaic', interactive=False,
       niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
@@ -106,7 +132,7 @@ for suffix in ('flux','psf','residual'):
     os.system('rm -rf test_frequency_mincontsub-linecontsub.{0}'.format(suffix))
 
 os.system('rm -rf test_mfs_mincontsub-linecontsub.*')
-clean(vis='w51_test_small_imcont-linecont.ms', imagename="test_mfs_mincontsub-linecontsub",
+clean(vis='w51_test_merge_spw3_copy_imcont-linecont.ms', imagename="test_mfs_mincontsub-linecontsub",
       field="", spw='', mode='mfs', outframe='LSRK',
       interpolation='linear', imagermode='mosaic', interactive=False,
       niter=10000, threshold='50.0mJy', imsize=[512,512], cell='0.052arcsec',
